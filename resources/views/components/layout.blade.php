403ddb82a096860785f7891d24ca4c42cc8d73cb
théoo <90846584+theokbokki@users.noreply.github.com>
2025-08-23 10:04:51 +0200

    Add metas and favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,27 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Quick thoughts and other stuff I didn't want to expand on.">
+        <meta name="keywords" content="feed, code, life, thoughts">
+        <meta name="author" content="Théoo">
+
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ Vite::asset('resources/favicons/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ Vite::asset('resources/favicons/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ Vite::asset('resources/favicons/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ Vite::asset('resources/favicons/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ Vite::asset('resources/favicons/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ Vite::asset('resources/favicons/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ Vite::asset('resources/favicons/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ Vite::asset('resources/favicons/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ Vite::asset('resources/favicons/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192" href="{{ Vite::asset('resources/favicons/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ Vite::asset('resources/favicons/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ Vite::asset('resources/favicons/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ Vite::asset('resources/favicons/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ Vite::asset('resources/favicons/manifest.json') }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="{{ Vite::asset('resources/favicons/ms-icon-144x144.png') }}">
+        <meta name="theme-color" content="#ffffff">
 
         <title>Feed</title>
 
