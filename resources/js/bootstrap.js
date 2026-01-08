d5286c7411b907736ae2ad21f0fa481831c059ca
théoo <90846584+theokbokki@users.noreply.github.com>
2026-01-08 10:53:59 +0100

    Add header

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,7 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import.meta.glob([
     "../favicons/**.svg",
     "../favicons/**.png",
     "../favicons/**.ico",
     "../favicons/**.webmanifest",
+    "../img/",
 ]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
-    <h1 class="sro">Théoo's feed</h1>
+    <header class="header">
+        <h1 class="sro">Théoo's feed</h1>
+        <div class="header__media">
+            <img class="header__img" src="{{ Vite::asset('resources/img/mii.webp') }}" alt="" />
+            <div class="header__text">
+                <p class="header__name">Théoo</p>
+                <a class="header__mail" href="mailto:hello@theoo.dev">hello@theoo.dev</a>
+            </div>
+        </div>
+    </header>
     <main class="app__main">
         <section class="posts">
             @foreach($groups as $date => $posts)
@@ -13,26 +22,26 @@
                 </div>
             @endforeach
         </section>
+        @auth()
+            <div id="preview" class="post__preview"></div>
+            <form id="postForm" action="{{ route('posts.create') }}" class="post__form">
+                @csrf
+                <label for="content" class="sro">New post</label>
+                <button type="button" id="addImageBtn" class="post__button post__button--img">
+                    <span class="sro">Add image</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+                </button>
+                <textarea name="content" id="content" class="post__textarea"></textarea>
+                <input type="file"
+                    id="fileInput"
+                    accept="image/*"
+                    capture
+                    hidden>
+                <button type="submit" class="post__button post__button--create">
+                    <span class="sro">Create</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-icon lucide-arrow-up"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
+                </button>
+            </form>
+        @endauth
     </main>
-    @auth()
-    <div id="preview" class="post__preview"></div>
-    <form id="postForm" action="{{ route('posts.create') }}" class="post__form">
-        @csrf
-        <label for="content" class="sro">New post</label>
-        <button type="button" id="addImageBtn" class="post__button post__button--img">
-            <span class="sro">Add image</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-        </button>
-        <textarea name="content" id="content" class="post__textarea"></textarea>
-        <input type="file"
-            id="fileInput"
-            accept="image/*"
-            capture
-            hidden>
-        <button type="submit" class="post__button post__button--create">
-            <span class="sro">Create</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-icon lucide-arrow-up"><path d="m5 12 7-7 7 7"/><path d="M12 19V5"/></svg>
-        </button>
-    </form>
-    @endauth
 </x-layout>
